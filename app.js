23810b9f6847df3e955802cff1d50c2d0173c2f0
Abdul Ahad Mansoori <109919558+abdulahadmansoori@users.noreply.github.com>
2023-02-05 02:02:29 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 $(document).ready(function () {
     let news = $(".news").html(); 
+    // .ajax({
+    //     type: "GET",
+    //     url: "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=29c9ac5ae717454592464a9538c0d6d4",
+    //     dataType: "JSON",
+    //     success: function (response) {
+    //         response.articles.forEach(element => {
+    //             $(".news").html(news += `
+    //             <div class='col-md-4'>
+    //                 <img class='w-100' src='${element.urlToImage}' />
+    //                 <h4>${element.title}</h4>
+    //                 <p>${element.content} <a href='${element.url}' target='_blank'>more</a></p>
+    //             </div>
+    //             `);
+    //             console.log(element);
+    //         });
+    //     },
+    //     error: function (response) {
+    //         console.log(response);
+    //     }
+    // });
+
     $.ajax({
         type: "GET",
-        url: "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=29c9ac5ae717454592464a9538c0d6d4",
+        url: 'http://api.mediastack.com/v1/news?access_key=64da30206689d07ca440165871fc3ae8&keywords=tennis&countries=us,gb,de',
         dataType: "JSON",
         success: function (response) {
-            response.articles.forEach(element => {
-                $(".news").html(news += `
-                <div class='col-md-4'>
-                    <img class='w-100' src='${element.urlToImage}' />
-                    <h4>${element.title}</h4>
-                    <p>${element.content} <a href='${element.url}' target='_blank'>more</a></p>
-                </div>
-                `);
-                console.log(element);
-            });
-        },
-        error: function (response) {
-            console.log(response);
+        console.log(response);
         }
     });
+
 });
