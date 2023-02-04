ebe1c4af03f1f9a7597fc03177eb9c5ee4580eab
Abdul Ahad Mansoori <109919558+abdulahadmansoori@users.noreply.github.com>
2023-02-05 02:08:26 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,5 @@
 $(document).ready(function () {
     let news = $(".news").html(); 
-    // .ajax({
-    //     type: "GET",
-    //     url: "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=29c9ac5ae717454592464a9538c0d6d4",
-    //     dataType: "JSON",
-    //     success: function (response) {
-    //         response.articles.forEach(element => {
-    //             $(".news").html(news += `
-    //             <div class='col-md-4'>
-    //                 <img class='w-100' src='${element.urlToImage}' />
-    //                 <h4>${element.title}</h4>
-    //                 <p>${element.content} <a href='${element.url}' target='_blank'>more</a></p>
-    //             </div>
-    //             `);
-    //             console.log(element);
-    //         });
-    //     },
-    //     error: function (response) {
-    //         console.log(response);
-    //     }
-    // });
 
     $.ajax({
         type: "GET",
